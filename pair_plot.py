265e446a9b1afcb745f33b4040ad2e7a02ca701a
U-LAPTOP-HV3ICQ68\Nicolas <nbrucker@student.42.fr>
2019-01-29 17:49:56 +0100

    pair_plot !

--- pair_plot.py
+++ pair_plot.py
@@ -14,9 +14,27 @@ def main():
 	except:
 		error.error('error opening file')
 	data = data.dropna()
+	houses = data['Hogwarts House']
 	data = data.drop(columns=['Index', 'Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand'])
-	sns.pairplot(data)
-
+	results = data.max() - data.min()
+	i = 0
+	while (i < len(results)):
+		if (results[i] == 0):
+			results[i] = 1
+		i += 1
+	data = (data - data.min()) / results
+	data['Houses'] = houses
+	color = {
+		'Gryffindor': 'red',
+		'Slytherin': 'green',
+		'Ravenclaw': 'blue',
+		'Hufflepuff': '#EBEB11'
+	}
+	options = {
+		'alpha': 0.6
+	}
+	plot = sns.pairplot(data, hue='Houses', palette=color, plot_kws=options)
+	plot.savefig("./pair_plot.png")
 
 if __name__ == '__main__':
 	main()
